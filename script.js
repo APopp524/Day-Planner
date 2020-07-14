c0180818c30f34b86ef6bf8204621514458526f6
Andrew Popp <apopp524@gmail.com>
2020-07-13 20:34:45 -0500

    updated everything

--- script.js
+++ script.js
@@ -1,91 +1,163 @@
 var dayPlanner = [
-{
-    id: "0",
-    hour: "09",
-    time: "09",
-    meridiem: "am",
-    reminder: ""
-},
 
-{
-    id: "1",
-    hour: "10",
-    time: "10",
-    meridiem: "am",
-    reminder: ""
-},
+    {
+        id: "0",
+        hour: "09",
+        time: "09",
+        meridiem: "am",
+        reminder: ""
+    },
 
-{
-    id: "2",
-    hour: "11",
-    time: "11",
-    meridiem: "am",
-    reminder: ""
-},
+    {
+        id: "1",
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        reminder: ""
+    },
 
-{
-    id: "3",
-    hour: "12",
-    time: "12",
-    meridiem: "pm",
-    reminder: ""
-},
+    {
+        id: "2",
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        reminder: ""
+    },
 
-{
-    id: "4",
-    hour: "01",
-    time: "13",
-    meridiem: "pm",
-    reminder: ""
-},
+    {
+        id: "3",
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "01",
+        time: "13",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "02",
+        time: "14",
+        meridiem: "pm",
+        reminder: ""
+    },
 
-{
-    id: "5",
-    hour: "02",
-    time: "14",
-    meridiem: "pm",
-    reminder: ""
-},
+    {
+        id: "6",
+        hour: "03",
+        time: "15",
+        meridiem: "pm",
+        reminder: ""
+    },
 
-{
-    id: "6",
-    hour: "03",
-    time: "15",
-    meridiem: "pm",
-    reminder: ""
-},
+    {
+        id: "7",
+        hour: "04",
+        time: "16",
+        meridiem: "pm",
+        reminder: ""
+    },
 
-{
-    id: "7",
-    hour: "04",
-    time: "16",
-    meridiem: "pm",
-    reminder: ""
-},
-
-{
-    id: "8",
-    hour: "05",
-    time: "17",
-    meridiem: "pm",
-    reminder: ""
-},
-
+    {
+        id: "8",
+        hour: "05",
+        time: "17",
+        meridiem: "pm",
+        reminder: ""
+    },
+    
 ]
 
-// Saves all the reminders and data to Local Storage
-function saveDate() {
+// localStorage saves 
+function plannerSaves() {
     localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
 }
 
-function showSaveDate() {
+
+function displaySaves() {
     dayPlanner.forEach(function (_thisHour) {
         $(`#${_thisHour.id}`).val(_thisHour.reminder);
     })
 }
 
-// Get updated current date 
-function currentDate() {
-    var topCurrentDate = moment().format('dddd, MMMM Do');
-    $("#currentDay").text(currentDate);
+function currentTimeDay() {
+    var currentTimeDay = moment().format('dddd, MMMM Do');
+    $("#currentDay").text(currentTimeDay);
 }
+
+
+function init() {
+    var daySaves = JSON.parse(localStorage.getItem("dayPlanner"));
+
+    if (daySaves) {
+        dayPlanner = daySaves;
+    }
+
+    plannerSaves();
+    displaySaves();
+}
+
+// loads current time and day moment.js
+currentTimeDay();
+
+dayPlanner.forEach(function(thisHour) {
+
+    var hourRow = $("<form>").attr({
+        "class": "row"
+    });
+    $(".container").append(hourRow);
+
+    var hourField = $("<div>")
+        .text(`${thisHour.hour}${thisHour.meridiem}`)
+        .attr({
+            "class": "col-md-2 hour"
+    });
+
+    // planner data
+    var hourPlan = $("<div>")
+        .attr({
+            "class": "col-md-9 description p-0"
+        });
+    var planData = $("<textarea>");
+    hourPlan.append(planData);
+    planData.attr("id", thisHour.id);
+    if (thisHour.time < moment().format("HH")) {
+        planData.attr ({
+            "class": "past", 
+        })
+    } else if (thisHour.time === moment().format("HH")) {
+        planData.attr({
+            "class": "present"
+        })
+    } else if (thisHour.time > moment().format("HH")) {
+        planData.attr({
+            "class": "future"
+        })
+    }
+
+    
+    // save button for each event
+    var saveButton = $("<i class='far fa-save fa-lg'></i>")
+    var savePlan = $("<button>")
+        .attr({
+            "class": "col-md-1 saveBtn"
+    });
+    savePlan.append(saveButton);
+    hourRow.append(hourField, hourPlan, savePlan);
+})
+
+// saves data to be used in localStorage
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var saveLocalStorage = $(this).siblings(".description").children(".future").attr("id");
+    dayPlanner[saveLocalStorage].reminder = $(this).siblings(".description").children(".future").val();
+    console.log(saveLocalStorage);
+    plannerSaves();
+    displaySaves();
+})
+
+init();
